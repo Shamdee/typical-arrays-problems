74f8048c1675e36df233ee6148d73998fceea877
Shamdee <shamdee@gmail.com>
2021-08-21 12:27:09 +0300

    Solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(typeof array != "undefined" && array != null && array.length > 0) {
+
+        let num = array[0]
+        for (let i = 0; i < array.length; i++)
+            if (num > array[i])
+                num = array[i]
+        return num
+    }
+        return 0
+
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(typeof array != "undefined" && array != null && array.length > 0) {
+        let num = array[0]
+        for (let i = 0; i < array.length; i++)
+            if (num < array[i])
+                num = array[i]
+        return num
+    }
+    return 0
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (typeof array != "undefined" && array != null && array.length > 0) {
+        let num = 0
+        for (let i = 0; i < array.length; i++)
+            num = num + array[i]
+        return num / array.length;
+    }
+    return 0
 }
+
+
+
